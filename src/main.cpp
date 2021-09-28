4c18a69e47860e47f149b8c506f8a390992550b2
Guilhem Mathieux <guilhem.mathieux@telecom-paris.fr>
2021-09-28 11:32:09 +0200

    testing step 4

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
-#include "Multimedia.h"
 #include <iostream>
 using namespace std;
 
-#define STEP 3
+#define STEP 4
+
+#include "Multimedia.h"
 
 #if STEP == 3
 int main(int argc, const char* argv[])
@@ -28,9 +29,23 @@ int main(int argc, const char* argv[])
 }
 
 #elif STEP == 4
+#include "Image.h"
+#include "Video.h"
+
 int main(int argc, const char* argv[])
 {
     cout << endl << "Testing Step " << STEP << endl << endl;
+    
+    Image *file1 = new Image ("image_test", "arco.jpg", 400, 100);
+    Video *file2 = new Video ("video_test", "woof.mp4", 14);
+    
+    cout << "file1" << endl;
+    file1->infos_out(cout);
+
+    cout <<  endl << "file2" << endl;
+    file2->infos_out(cout);
+
+    return 0;
 }
 
 #endif
