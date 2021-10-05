ffbfdbf34d6e8b13e23f40436d1f06f7b461edf6
Guilhem Mathieux <guilhem.mathieux@telecom-paris.fr>
2021-10-05 09:39:30 +0200

    step 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
 
-#define STEP 4
+#define STEP 5
 
 #include "Multimedia.h"
+
+#define IMG_PATH "res/arco.jpg"
+#define VID_PATH "res/woof.mp4"
 
 #if STEP == 3
 int main(int argc, const char* argv[])
@@ -36,17 +39,43 @@ int main(int argc, const char* argv[])
 {
     cout << endl << "Testing Step " << STEP << endl << endl;
     
-    Image *file1 = new Image ("image_test", "arco.jpg", 400, 100);
-    Video *file2 = new Video ("video_test", "woof.mp4", 14);
+    Image *file1 = new Image ("image_test", IMG_PATH, 400, 100);
+    Video *file2 = new Video ("video_test", VID_PATH, 14);
     
-    cout << "file1" << endl;
+    cout << endl << "file1" << endl;
     file1->infos_out(cout);
 
     cout <<  endl << "file2" << endl;
     file2->infos_out(cout);
 
     return 0;
+    // method open is declared as abstract (virtual) in multimedia
 }
 
+#elif STEP == 5
+#include "Image.h"
+#include "Video.h"    
+
+int main(int argc, const char* argv[])
+{
+    cout << endl << "Testing Step " << STEP << endl << endl;
+
+    //creating tab
+    Multimedia ** file = new Multimedia * [4];
+    file[0] = new Image ("first image", IMG_PATH, 400, 100);
+    file[1] = new Video ("first video", VID_PATH, 14);
+    file[2] = new Image("second image","place_holder.jpg",800,100);
+    file[3] = new Video("second video","place_holder.mp4",203);
+
+    // diplaying infos
+    for (int i=0; i<4; i++)
+    {
+        cout <<  endl << "file n°" << i+1 << endl;
+        file[i]->infos_out(cout);
+    }
+ 
+    return 0;
+} 
 #endif
 
+ 
