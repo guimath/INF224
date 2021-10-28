8dd1a8b29dc30cc6d0d81f245131b95f93f8c921
Guilhem Mathieux <guilhem.mathieux@telecom-paris.fr>
2021-10-28 18:00:24 +0200

    added group

---
+++ src/Group.cpp
@@ -0,0 +1,30 @@
+#include "Group.h"
+
+using namespace std;
+
+
+Group::Group(string group_name):list<Multimedia*>()
+{
+    m_group_name = group_name;
+}
+
+string Group::get_group_name() const
+{
+    return m_group_name;
+}
+
+void Group::set_group_name(string group_name) 
+{
+    m_group_name = group_name;
+}
+
+void Group::infos_out(ostream &out) 
+{
+    out << "info for group named \"" << m_group_name << "\" :" << endl;
+    int i =1;
+    for (Group :: iterator file = this->begin(); file != this->end(); file++){
+        out <<  endl << "file n°" << i++ << endl;  
+        
+        (*file)->infos_out(out);
+    }
+}
---
+++ src/Group.h
@@ -0,0 +1,48 @@
+#ifndef DEF_GROUP
+#define DEF_GROUP
+
+#include "Multimedia.h"
+
+#include <list>
+/**
+ *	@brief A class that represents a group of multimedia files 
+ *	
+ *	@author guimath
+ */
+class Group : public std::list<Multimedia*>
+{
+private:
+    /** Group name */
+    std::string m_group_name;
+
+public:
+    /**
+     * @brief Construct a new Group object
+     * 
+     * @param group_name The name of the group
+     */
+    Group(std::string group_name);
+
+    /**
+     * @brief Get the group_name instance
+     * 
+     * @return the group_name instance
+     */
+    std::string get_group_name() const;
+
+    /**
+     * @brief Set the group_name instance
+     * 
+     * @param group_name the new name of group
+     */
+    void set_group_name(std::string group_name);
+
+    /**
+     * @brief prints infos of the object to the given output
+     * 
+     * @param out outstream where info will be printed
+     */
+    void infos_out(std::ostream &out);
+};
+
+#endif
